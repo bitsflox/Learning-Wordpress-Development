5d4514518a6dd78d08420bc122ebc2a22651b95f
bitsflox <rawat_anil4@hotmail.com>
2018-08-28 18:43:01 +0530

    today learing

--- wp-content/themes/my_blog/functions.php
+++ wp-content/themes/my_blog/functions.php
@@ -40,3 +40,32 @@ function has_children(){
 
 	return count($pages);
 }
+
+// Customixe execrpt word count length
+
+function custom_excerpt_lenght(){
+	return 25;
+}
+
+add_filter('excerpt_length','custom_excerpt_lenght');
+
+
+// Theme Setup
+function learningWordPress_setup(){
+
+	// Navigation Menus
+
+register_nav_menus(array(
+	'primary' => __('Primary Menu'),
+	'footer' => __('Footer Menu'),
+	));
+
+	// Add feature image support
+	add_theme_support('post-thumbnails');
+	add_image_size('small-thumbnail', 180, 120, true);
+	add_image_size('banner-image', 920, 210, array('left','top'));
+
+}
+
+
+add_action('after_setup_theme','learningWordPress_setup');
